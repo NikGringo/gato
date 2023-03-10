e92f1bb2d009ad73cdea3c2e15ef0e234d31f8b7
Nik Gringo <jeremie@bk.ru>
2023-03-10 16:25:16 +0500

    Rename number column

--- app/components/table.jsx
+++ app/components/table.jsx
@@ -1,7 +1,7 @@
 const React = require("react");
 
 function Table(props) {
-    const columns = ["Номер", "Название", "Продолжительность"]
+    const columns = ["№", "Название", "Продолжительность"]
     const data = [
         { title: "The last of us", duration: "01:30" },
         { title: "Wensday", duration: "01:30" },
