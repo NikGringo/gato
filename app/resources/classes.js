b1bf560757228923a89b6dd50a8616b3c8d2d7b4
NikGringo <jeremie@bk.ru>
2023-04-12 23:16:51 +0500

    Added 3 week of classes

--- app/resources/classes.js
+++ app/resources/classes.js
@@ -371,5 +371,270 @@ export const classes = [
                 cabinet: ""
             },
         ]
+    },
+    {
+        day: "Понедельник 17.04.2023",
+        classes: [
+            {
+                number: 1,
+                time: "08:30 - 10:00",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 2,
+                time: "10:10 - 11:40",
+                name: "УИР (практика)",
+                cabinet: "28"
+            },
+            {
+                number: 3,
+                time: "12:10 - 13:40",
+                name: "УИР (КСР, диф)",
+                cabinet: "28"
+            },
+            {
+                number: 4,
+                time: "14:00 - 15:30",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 5,
+                time: "15:40 - 17:10",
+                name: "Функциональное программирование",
+                cabinet: "29"
+            },
+            {
+                number: 6,
+                time: "17:20 - 18:30",
+                name: "Функциональное программирование",
+                cabinet: "29"
+            },
+            {
+                number: 7,
+                time: "18:30 - 20:00",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 8,
+                time: "20:10 - 21:40",
+                name: "-",
+                cabinet: ""
+            },
+        ]
+    },
+    {
+        day: "Вторник 18.04.2023",
+        classes: [
+            {
+                number: 1,
+                time: "08:30 - 10:00",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 2,
+                time: "10:10 - 11:40",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 3,
+                time: "12:10 - 13:40",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 4,
+                time: "14:00 - 15:30",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 5,
+                time: "15:40 - 17:10",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 6,
+                time: "17:20 - 18:30",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 7,
+                time: "18:30 - 20:00",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 8,
+                time: "20:10 - 21:40",
+                name: "-",
+                cabinet: ""
+            },
+        ]
+    },
+    {
+        day: "Среда 19.04.2023",
+        classes: [
+            {
+                number: 1,
+                time: "08:30 - 10:00",
+                name: "Функциональное программирование",
+                cabinet: "29"
+            },
+            {
+                number: 2,
+                time: "10:10 - 11:40",
+                name: "Функциональное программирование",
+                cabinet: "29"
+            },
+            {
+                number: 3,
+                time: "12:10 - 13:40",
+                name: "ООП",
+                cabinet: "29"
+            },
+            {
+                number: 4,
+                time: "14:00 - 15:30",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 5,
+                time: "15:40 - 17:10",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 6,
+                time: "17:20 - 18:30",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 7,
+                time: "18:30 - 20:00",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 8,
+                time: "20:10 - 21:40",
+                name: "-",
+                cabinet: ""
+            },
+        ]
+    },
+    {
+        day: "Четверг 20.04.2023",
+        classes: [
+            {
+                number: 1,
+                time: "08:30 - 10:00",
+                name: "Администрирование систем",
+                cabinet: "29"
+            },
+            {
+                number: 2,
+                time: "10:10 - 11:40",
+                name: "Администрирование систем",
+                cabinet: "29"
+            },
+            {
+                number: 3,
+                time: "12:10 - 13:40",
+                name: "Администрирование систем",
+                cabinet: "29"
+            },
+            {
+                number: 4,
+                time: "14:00 - 15:30",
+                name: "Современные инструменты бизнес-процессов",
+                cabinet: "28"
+            },
+            {
+                number: 5,
+                time: "15:40 - 17:10",
+                name: "Современные инструменты бизнес-процессов",
+                cabinet: "28"
+            },
+            {
+                number: 6,
+                time: "17:20 - 18:30",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 7,
+                time: "18:30 - 20:00",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 8,
+                time: "20:10 - 21:40",
+                name: "-",
+                cabinet: ""
+            },
+        ]
+    },
+    {
+        day: "Пятница 21.04.2023",
+        classes: [
+            {
+                number: 1,
+                time: "08:30 - 10:00",
+                name: "Администрирование систем",
+                cabinet: "29"
+            },
+            {
+                number: 2,
+                time: "10:10 - 11:40",
+                name: "Администрирование систем",
+                cabinet: "29"
+            },
+            {
+                number: 3,
+                time: "12:10 - 13:40",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 4,
+                time: "14:00 - 15:30",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 5,
+                time: "15:40 - 17:10",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 6,
+                time: "17:20 - 18:30",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 7,
+                time: "18:30 - 20:00",
+                name: "-",
+                cabinet: ""
+            },
+            {
+                number: 8,
+                time: "20:10 - 21:40",
+                name: "-",
+                cabinet: ""
+            },
+        ]
     }
 ]
